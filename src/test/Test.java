c0eee40fbc584661a5aa59b243a7fac0a2d9e39d
sonpd1 <phamducson2009@gmail.com>
2017-07-14 22:46:03 +0700

    sonpd

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("committed by sonpd");
+		System.out.println("committed by sonpd to master");
 	}
 
 }
